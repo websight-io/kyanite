2bd068f01f458b7a3137a088d3842d82cebfea48
nowak-pawel-ds <110535080+nowak-pawel-ds@users.noreply.github.com>
2022-08-22 08:46:59 +0200

    Feature/hl-333 Add Image Component (#2)
    * HL-333 Create first scrach of image component
    
    * HL-333 Image component almost finished
    
    * HL-333 Add some adjustments
    
    * HL-333 Add unit tests for Image Component

---
+++ core/src/main/java/pl/ds/bulma/components/models/ImageComponent.java
@@ -0,0 +1,43 @@
+package pl.ds.bulma.components.models;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+import lombok.Getter;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
+
+@Model(adaptables = Resource.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+public class ImageComponent {
+
+  @Inject
+  @Getter
+  private String src;
+
+  @Inject
+  @Getter
+  private String assetReference;
+
+  @Inject
+  @Getter
+  private String type;
+
+  @Inject
+  @Getter
+  private String style;
+
+  @Inject
+  @Getter
+  private String isRounded;
+
+  @Inject
+  @Getter
+  private String alt;
+
+  @PostConstruct
+  private void init() {
+    if (assetReference == null && src != null) {
+      assetReference = src;
+     }
+  }
+}
