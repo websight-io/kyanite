d304d2a292115578a288e0ecfca5bb645ce86db7
Paweł Jóźwicki <pawel.jozwicki@ds.pl>
2022-10-24 12:58:26 +0200

    Removing test with menu edition

--- tests/end-to-end/tests/menu.cy.ts
+++ tests/end-to-end/tests/menu.cy.ts
@@ -31,44 +31,5 @@ describe('Menu component', function () {
 
     cy.contains('Second item').click();
     cy.contains('First label').click();
-
-  });
-
-  it('renders correctly in edit mode', function () {
-    cy.intercept(
-        'POST',
-        '**/pagecontainer/menu.websight-dialogs-service.save-properties.action'
-    ).as('saveProperties');
-
-    cy.visit(
-        '/apps/websight/index.html/content/bulma-tests/pages/menu::editor'
-    );
-
-    cy.getByTestId(paths.menu)
-    .click()
-    .find(selectors.overlayName)
-    .should('have.text', 'Menu');
-
-    cy.percySnapshotPageEditor('Menu editor');
-
-    cy.getByTestId(testIds.editIcon).click();
-
-    cy.getByTestId('Input_Sectionlabel').clear().type('Edited section');
-    cy.getByTestId('Input_Itemtext').clear().type('Subtitle');
-
-    cy.percySnapshotDialog('Menu dialog');
-
-    cy.getByTestId(testIds.dialogSubmitButton).click();
-
-    cy.wait('@saveProperties');
-
-    cy.request(
-        '/content/bulma-tests/pages/menu/jcr:content/pagecontainer/menu.json'
-    )
-    .its('body')
-    .should('deep.eq', {
-      'sling:resourceType': 'bulma/components/menu',
-      'jcr:primaryType': 'nt:unstructured'
-    });
   });
 });
