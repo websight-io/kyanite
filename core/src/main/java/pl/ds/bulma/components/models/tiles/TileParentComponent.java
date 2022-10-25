df28e493702b520a56eb9c6e0aedc3332c472c4b
Piotr Szewczul <piotr.szewczul@ds.pl>
2022-10-25 13:04:18 +0200

    separate parent and child component

--- core/src/main/java/pl/ds/bulma/components/models/tiles/TileParentComponent.java
+++ core/src/main/java/pl/ds/bulma/components/models/tiles/TileParentComponent.java
@@ -18,24 +18,66 @@
 
 package pl.ds.bulma.components.models.tiles;
 
+
 import static org.apache.sling.models.annotations.DefaultInjectionStrategy.OPTIONAL;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 import lombok.Getter;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.Default;
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.injectorspecific.SlingObject;
 
 @Model(adaptables = Resource.class, defaultInjectionStrategy = OPTIONAL)
-public class TilesComponent {
+public class TileParentComponent {
+
+  private static final String PARENT_TYPE = "is-parent";
+  private static final String IS_VERTICAL = "is-vertical";
+  private static final String CHILD_RESOURCE_TYPE = "bulma/components/tiles/tilechild";
 
   @SlingObject
   private Resource resource;
 
+  @Inject
+  @Getter
+  @Default(booleanValues = false)
+  private boolean isVertical;
+
+  @Inject
+  @Getter
+  @Default(values = StringUtils.EMPTY)
+  private String size;
+
+  @Inject
+  @Getter
+  private String[] classes;
+
+  @PostConstruct
+  private void init() {
+    List<String> classList = new ArrayList<>();
+
+    if (StringUtils.isNotEmpty(size)) {
+      classList.add(size);
+    }
+    if (isVertical) {
+      classList.add(IS_VERTICAL);
+    }
+
+    boolean allChildren = isAllChildren();
+    if (allChildren) {
+      classList.add(PARENT_TYPE);
+    }
+
+    classes = classList.toArray(new String[]{});
+  }
+
+  private boolean isAllChildren() {
+    return StreamSupport.stream(resource.getChildren().spliterator(), false)
+        .allMatch(res -> CHILD_RESOURCE_TYPE.equals(res.getResourceType()));
+  }
 }
