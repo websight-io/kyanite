51a5903e93a6cb394099684077cfc8afecaa2be6
Gergo Fazekas <gergo.fazekas@ds.pl>
2023-01-04 08:35:28 +0100

    test coverage for add table column

--- application/bulma-table-service/src/test/java/pl/ds/bulma/rest/table/AddTableColumnRestModelTest.java
+++ application/bulma-table-service/src/test/java/pl/ds/bulma/rest/table/AddTableColumnRestModelTest.java
@@ -17,15 +17,21 @@
 package pl.ds.bulma.rest.table;
 
 import static java.util.Objects.requireNonNull;
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.servlets.HttpConstants;
 import org.apache.sling.testing.mock.sling.ResourceResolverType;
 import org.apache.sling.testing.mock.sling.junit5.SlingContext;
 import org.apache.sling.testing.mock.sling.junit5.SlingContextExtension;
+import org.apache.sling.testing.mock.sling.servlet.MockSlingHttpServletRequest;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import pl.ds.websight.request.parameters.support.impl.injectors.RequestParameterInjector;
 
 @ExtendWith(SlingContextExtension.class)
 public class AddTableColumnRestModelTest {
@@ -34,19 +40,33 @@ public class AddTableColumnRestModelTest {
 
   private final SlingContext context = new SlingContext(ResourceResolverType.RESOURCERESOLVER_MOCK);
 
+  private MockSlingHttpServletRequest request;
+
   @BeforeEach
   public void init() {
+    context.registerService(new RequestParameterInjector());
     context.addModelsForClasses(AddTableColumnRestModel.class);
     context.load().json(requireNonNull(
         Thread.currentThread().getContextClassLoader().getResourceAsStream("table.json")), PATH);
+    request = context.request();
+    request.setMethod(HttpConstants.METHOD_POST);
   }
 
   @Test
   void addTableColumnRestModelTest() {
-    AddTableColumnRestModel model = requireNonNull(
-        context.resourceResolver().getResource(PATH + "/default")).adaptTo(AddTableColumnRestModel.class);
+    request.setResource(
+        context.resourceResolver().getResource("/content/table/complex/tablerow1/tablecell1"));
+    AddTableColumnRestModel model = request.adaptTo(AddTableColumnRestModel.class);
+    List<String> expected = Arrays.asList(
+        "/content/table/complex/tablerow1",
+        "/content/table/complex/tablehead/tablerow1",
+        "/content/table/complex/tablebody/tablerow1",
+        "/content/table/complex/tablefoot/tablerow1"
+    );
 
-    assertThat(model).isNotNull();
+    List<String> actual = model.getRows().stream().map(Resource::getPath)
+        .collect(Collectors.toList());
+    assertEquals(expected, actual);
   }
 
 }
