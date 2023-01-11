5080bff15b23a354c6603bd9e938186a15d34b2b
Gergo Fazekas <gergo.fazekas@ds.pl>
2023-01-11 08:24:56 +0100

    add custom rules

--- application/bulma-table-view/.eslintrc.js
+++ application/bulma-table-view/.eslintrc.js
@@ -19,10 +19,6 @@ module.exports = {
     'browser': true,
     'es2021': true,
   },
-  'extends': [
-    'plugin:react/recommended',
-    'google',
-  ],
   'overrides': [
   ],
   'parserOptions': {
@@ -32,6 +28,20 @@ module.exports = {
   'plugins': [
     'react',
   ],
-  'rules': {
+  rules:  {
+    'curly': 1,
+    '@typescript-eslint/explicit-function-return-type': [0],
+    '@typescript-eslint/no-explicit-any': [0],
+    'ordered-imports': [0],
+    'object-literal-sort-keys': [0],
+    'max-len': [1, 120],
+    'new-parens': 1,
+    'no-bitwise': 1,
+    'no-cond-assign': 1,
+    'no-trailing-spaces': 0,
+    'eol-last': 1,
+    'func-style': ['error', 'declaration', { 'allowArrowFunctions': true }],
+    'semi': 1,
+    'no-var': 0
   },
 };
