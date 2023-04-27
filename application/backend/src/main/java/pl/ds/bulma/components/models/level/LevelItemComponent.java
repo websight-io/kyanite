d4e1ff02bf2e23c7d7823139091f017a434bbed2
Rafał Ziemniewski <rafal.ziemniewski@ds.pl>
2023-04-27 17:35:00 +0200

    #164 optimize imports

--- application/backend/src/main/java/pl/ds/bulma/components/models/level/LevelItemComponent.java
+++ application/backend/src/main/java/pl/ds/bulma/components/models/level/LevelItemComponent.java
@@ -16,15 +16,14 @@
 
 package pl.ds.bulma.components.models.level;
 
-import javax.annotation.PostConstruct;
-import javax.inject.Inject;
 import lombok.Getter;
-import org.apache.commons.lang3.StringUtils;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.Default;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.injectorspecific.SlingObject;
+
+import javax.inject.Inject;
 
 @Model(adaptables = Resource.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
 public class LevelItemComponent {
