948dc5391d44808c374c4730e07767366b28a3e7
martyna-ds <120561838+martyna-ds@users.noreply.github.com>
2023-05-09 12:24:43 +0200

    168 rte text color extension (#175)
    * #165 Added icon position property to the button component adn adjuste… (#166)
    
    * #165 Button component: added possibility to define two icons (left and right)
    
    * #168 Add initial files
    
    * #168 Add configuration files
    
    * #168 Add color extension - draft, correct pom.xml
    
    * #145 created extensions module and moved tableservice and tableview i… (#167)
    
    * #145 created extensions module and moved tableservice and tableview into it
    
    * #145 updated readme file
    
    * #163 Added new button modifier option - inverted button (#169)
    
    #163 Button component:  added new modifier option - inverted button
    
    * #81 Added new breadcrumb element property - icon (#170)
    
    #81 Breadcrumb property: added new breadcrumb element property - icon
    
    * #168 Correct configuration
    
    * #168 Add new bulma-color module
    
    * #168 Add new bulma-color module
    
    * #168 Toggiling state
    
    * #168 ColorDialog
    
    * #164 added alignment config option for level item component (#171)
    
    * #164 added alignment config option for level item component
    
    * #164 now the alignment works in nested containers too
    
    * #164 unused constant
    
    * #164 optimize imports
    
    * #164 import order
    
    * WOS-168 Content Text weight (#173)
    
    * #168 Colors list - dynamic css creation
    
    * #168 Colors list - dynamic css creation
    
    * Wos 173 apps to libs (#180)
    
    * WOS-173 move apps -> libs
    
    * WOS-173 paths in sources apps -> libs
    
    * WOS-178 init content for hero component (#181)
    
    * WOS-173 move apps -> libs
    
    * WOS-173 paths in sources apps -> libs
    
    * WOS-178 init content for hero component
    
    * WOS-176 added container style config to navbar component (#182)
    
    * WOS-173 move apps -> libs
    
    * WOS-173 paths in sources apps -> libs
    
    * WOS-176 added container style config to navbar component
    
    * WOS-176 change after review
    
    * #170 added vertical alignment for level item component (#177)
    
    * #168 Move color extension to extensions folder
    
    * WOS-170 merge fix
    
    * WOS-170 one more fix
    
    * #168 Change /apps to /libs folder name
    
    * #168 Move configuration to libs in backend
    
    * #168 Move rte configuration to common
    
    * #168 Corrections after pr, bulma fix
    
    ---------
    
    Co-authored-by: AdrianStaskiewicz <34780625+AdrianStaskiewicz@users.noreply.github.com>
    Co-authored-by: Rafal-DS <121929316+Rafal-DS@users.noreply.github.com>
    Co-authored-by: andrechern <119608077+andrechern@users.noreply.github.com>
    Co-authored-by: Rafał Ziemniewski <rafal.ziemniewski@ds.pl>
    Co-authored-by: Paweł Jóźwicki <pawel.jozwicki@ds.pl>

---
+++ application/extensions/bulma-color/src/main/webapp/libs/bulma-color/web-resources/components/richtext/ui/ColorDialogStyles.ts
@@ -0,0 +1,68 @@
+/* eslint-disable max-len */
+/*
+ * Copyright (C) 2023 Dynamic Solutions
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import styled, {css} from 'styled-components';
+
+export const createGlobalCSS = (colors) => {
+    let styles = '';
+
+    for (const color of colors) {
+       styles += `
+         .${color.colorClassName} {
+            color: ${color.value};
+         }
+       `;
+    }
+  
+    return css`${styles}`;
+};
+
+export const createCSS = (colors) => {
+    let styles = '';
+
+    for (const color of colors) {
+       styles += `
+         .${color.colorClassName} {
+            & + svg rect:first-of-type {
+                stroke: ${color.value};
+            }
+            & + svg g {
+                color: ${color.value};
+            }
+            & + svg {
+                background-color: ${color.value};
+            }
+         }
+       `;
+    }
+  
+    return css`${styles}`;
+};
+
+export const PopupContainer = styled.div`
+    & > div > div {
+        padding: 0;
+        display: flex;
+        flex-wrap: wrap;
+        & > button {
+            &:after {
+                font-family: Material Icons Outlined;
+                content: '\\e313';
+            }
+        }
+    }
+`;
