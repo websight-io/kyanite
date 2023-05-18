dfeefcd56007299e567a79b08ea92fa8088f8b5a
Rafal-DS <121929316+Rafal-DS@users.noreply.github.com>
2023-05-18 12:50:12 +0200

    WOS-195 Image component can now display videos (#200)
    * WOS-195 Image component can now display videos
    
    * WOS-195 getting file type by mimetype

--- application/backend/src/main/java/pl/ds/bulma/components/models/ImageComponent.java
+++ application/backend/src/main/java/pl/ds/bulma/components/models/ImageComponent.java
@@ -16,6 +16,7 @@
 
 package pl.ds.bulma.components.models;
 
+import java.net.URLConnection;
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 import lombok.Getter;
@@ -66,5 +67,10 @@ public class ImageComponent {
 
   public String getAssetReference() {
     return LinkUtil.handleLink(assetReference, resource.getResourceResolver());
+  }
+
+  public boolean getIsVideo() {
+    String mimeType = URLConnection.guessContentTypeFromName(getSrc());
+    return mimeType != null && mimeType.startsWith("video");
   }
 }
