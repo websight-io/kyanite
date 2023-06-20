d8a0729707f6e1ddb594b005f8a5dfa0c6ec7603
Andrei Charniauski <andrei.charniauski@ds.pl>
2023-06-20 08:47:23 +0200

    WOS-198 Create breadcrumb item component and implement icon feature support Fix tests

--- application/backend/src/test/java/pl/ds/bulma/components/models/BreadcrumbComponentTest.java
+++ application/backend/src/test/java/pl/ds/bulma/components/models/BreadcrumbComponentTest.java
@@ -25,22 +25,29 @@ import org.apache.sling.testing.mock.sling.junit5.SlingContextExtension;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import pl.ds.bulma.components.helpers.IconService;
 
-@ExtendWith(SlingContextExtension.class)
+@ExtendWith({SlingContextExtension.class, MockitoExtension.class})
 class BreadcrumbComponentTest {
+
+  @Mock
+  private IconService iconService;
 
   private static final String PATH = "/content/breadcrumb";
   private final SlingContext context = new SlingContext(ResourceResolverType.RESOURCERESOLVER_MOCK);
 
   @BeforeEach
   public void init() {
+    context.registerService(IconService.class, iconService);
     context.addModelsForClasses(BreadcrumbComponent.class);
     context.load().json(requireNonNull(
             Thread.currentThread().getContextClassLoader().getResourceAsStream("breadcrumb.json")),
         PATH);
   }
 
-//  @Test
+  @Test
   void defaultBreadcrumbComponentModelTest() {
     BreadcrumbComponent model = context.resourceResolver().getResource(PATH + "/default")
         .adaptTo(BreadcrumbComponent.class);
@@ -50,10 +57,9 @@ class BreadcrumbComponentTest {
     assertThat(model.getSize()).isEmpty();
     assertThat(model.getSeparator()).isEmpty();
     assertThat(model.getClasses()).isEmpty();
-//    assertThat(model.getElements()).isNull();
   }
 
-//  @Test
+  @Test
   void breadcrumbComponentModelTest() {
     BreadcrumbComponent model = context.resourceResolver().getResource(PATH + "/complex")
         .adaptTo(BreadcrumbComponent.class);
@@ -63,10 +69,5 @@ class BreadcrumbComponentTest {
     assertThat(model.getSeparator()).isEqualTo("has-bullet-separator");
     assertThat(model.getClasses()).containsExactlyInAnyOrder("center", "is-small",
         "has-bullet-separator");
-//    assertThat(model.getElements().stream()
-//        .filter(i -> i.getLabel().equals("Test1"))
-//        .count()).isEqualTo(1);
-//    assertThat(model.getElements().stream()
-//        .filter(i -> i.getLabel().equals("Test2")));
   }
 }
