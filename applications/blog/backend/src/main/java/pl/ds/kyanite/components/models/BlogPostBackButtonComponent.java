6d9bc45b87625dcaa1a8253c57748448ef981cf2
Rafał Ziemniewski <rafal.ziemniewski@ds.pl>
2023-07-13 15:45:26 +0200

    move application -> applications

--- applications/blog/backend/src/main/java/pl/ds/kyanite/components/models/BlogPostBackButtonComponent.java
+++ applications/blog/backend/src/main/java/pl/ds/kyanite/components/models/BlogPostBackButtonComponent.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2023 Dynamic Solutions
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package pl.ds.kyanite.components.models;
 
 import java.util.Objects;
@@ -15,13 +31,12 @@ import pl.ds.kyanite.components.utils.LinkUtil;
 import pl.ds.websight.pages.core.api.Page;
 import pl.ds.websight.pages.core.api.PageManager;
 
-
 @Model(
     adaptables = {Resource.class},
     defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL
 )
 public class BlogPostBackButtonComponent {
-    public static final String JCR_CONTENT = "/jcr:content";
+  public static final String JCR_CONTENT = "/jcr:content";
   private final ResourceResolver resourceResolver;
   private final Resource resource;
 
