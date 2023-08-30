a8805d355a17f7432db5ff2fac9f349935d19f53
Mikołaj Waśniewski <wasniewskimikolaj@gmail.com>
2023-08-30 11:17:32 +0200

    add missing licenses

--- tests/end-to-end/backstop_data/ci/provide-report.sh
+++ tests/end-to-end/backstop_data/ci/provide-report.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/bash
 
+#
+# Copyright (C) 2023 Dynamic Solutions
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 # Copies only required files as report to:
 # 1. use less space by copying just what is needed
 # 2. use easier artifact path in CI artifact config so downloaded report is not few directories "deep"
