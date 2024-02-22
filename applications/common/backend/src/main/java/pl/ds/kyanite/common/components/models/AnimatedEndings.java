1b066fa34c4b50d10f97d05f78a946f862bffa8a
Martyna Szeszko <martyna.szeszko@ds.pl>
2024-02-22 11:41:49 +0100

    KYAN-147 license minor fix

--- applications/common/backend/src/main/java/pl/ds/kyanite/common/components/models/AnimatedEndings.java
+++ applications/common/backend/src/main/java/pl/ds/kyanite/common/components/models/AnimatedEndings.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2023 Dynamic Solutions
+ * Copyright (C) 2024 Dynamic Solutions
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
