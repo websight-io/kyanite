d36c187cf353d4125f9ca82b9ea396be8e1b722e
Martyna Szeszko <martyna.szeszko@ds.pl>
2024-03-15 13:12:40 +0100

    KYAN-161 megamenu hovering support

--- applications/common/frontend/src/components/Navbar/navbar.ts
+++ applications/common/frontend/src/components/Navbar/navbar.ts
@@ -14,72 +14,81 @@
  * limitations under the License.
  */
 
-import { breakpoints } from "src/helpers/breakpoints";
+import { breakpoints } from 'src/helpers/breakpoints';
 
 document.addEventListener('scroll', () => {
-    const navbar = document.querySelector('.navbar');
-    const isTransparent = navbar.classList.contains('is-transparent');
-    if (isTransparent) {
-        if (window.scrollY > 10) {
-            navbar.classList.add('is-scrolled');
-        } else {
-            navbar.classList.remove('is-scrolled');
-        }
+  const navbar = document.querySelector('.navbar');
+  const isTransparent = navbar.classList.contains('is-transparent');
+  if (isTransparent) {
+    if (window.scrollY > 10) {
+      navbar.classList.add('is-scrolled');
+    } else {
+      navbar.classList.remove('is-scrolled');
     }
-})
+  }
+});
+
+// window.addEventListener('resize', () => {
+//   const desktopMQ = window.matchMedia(`(min-width: ${breakpoints.lg}px)`);
+//   handleMegamenuHover(desktopMQ.matches);
+// });
+
 document.addEventListener(window.KYANITE_ON_DOM_CONTENT_LOAD, () => {
+  handleFixedNavbarPosition();
 
-    handleFixedNavbarPosition();
+  // Get all "navbar-burger" elements
+  const $navbarBurgers = Array.prototype.slice.call(
+    document.querySelectorAll('.navbar-burger'),
+    0
+  );
 
-    // Get all "navbar-burger" elements
-    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+  // Add a click event on each of them
+  $navbarBurgers.forEach((el) => {
+    el.addEventListener('click', () => {
+      // Get the target from the "data-target" attribute
+      const target = el.dataset.target;
+      const $target = document.getElementById(target);
 
-    // Add a click event on each of them
-    $navbarBurgers.forEach( el => {
-        el.addEventListener('click', () => {
-
-            // Get the target from the "data-target" attribute
-            const target = el.dataset.target;
-            const $target = document.getElementById(target);
-
-            // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-            el.classList.toggle('is-active');
-            $target.classList.toggle('is-active');
-
-        });
+      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+      el.classList.toggle('is-active');
+      $target.classList.toggle('is-active');
     });
+  });
 
-    // Get all dropdowns items
-    const $navbarMega = Array.prototype.slice.call(document.querySelectorAll('.navbar-end .navbar-item.has-dropdown'));
-    const desktopMQ = window.matchMedia(`(min-width: ${breakpoints.lg}px)`);
+  // Get all dropdowns items
+  const $navbarMega = Array.prototype.slice.call(
+    document.querySelectorAll('.navbar-end .navbar-item.has-dropdown')
+  );
+  const desktopMQ = window.matchMedia(`(min-width: ${breakpoints.lg}px)`);
 
-    const onClick = (el) => {
-        el.target.parentElement.classList.toggle('is-active');
+  const onClick = (el) => {
+    el.target.parentElement.classList.toggle('is-active');
+  };
+
+  const actionForNavbarMega = (desktopMQ) => {
+    handleMegamenuHover(desktopMQ.matches);
+    // On desktop the dropdowns are hoverable
+    if (desktopMQ.matches) {
+      $navbarMega.forEach((el) => {
+        el.classList.add('is-hoverable');
+        el.classList.remove('is-active');
+        el.removeEventListener('click', onClick);
+      });
+
+      return;
     }
 
-    const actionForNavbarMega = (desktopMQ) => {
-        // On desktop the dropdowns are hoverable
-        if (desktopMQ.matches) {
-            $navbarMega.forEach((el) => {
-                el.classList.add('is-hoverable');
-                el.classList.remove('is-active');
-                el.removeEventListener('click', onClick);
-            });
+    // On mobile the dropdowns are clickable
+    $navbarMega.forEach((el) => {
+      el.classList.remove('is-hoverable');
+      el.classList.remove('is-active');
+      el.addEventListener('click', onClick);
+    });
+  };
 
-            return;
-        }
+  actionForNavbarMega(desktopMQ);
 
-        // On mobile the dropdowns are clickable
-        $navbarMega.forEach((el) => {
-            el.classList.remove('is-hoverable');
-            el.classList.remove('is-active');
-            el.addEventListener('click', onClick);
-        });
-    }
-
-    actionForNavbarMega(desktopMQ);
-
-    desktopMQ.addEventListener('change', () => actionForNavbarMega(desktopMQ));
+  desktopMQ.addEventListener('change', () => actionForNavbarMega(desktopMQ));
 });
 
 /**
@@ -94,14 +103,72 @@ document.addEventListener(window.KYANITE_ON_DOM_CONTENT_LOAD, () => {
  * Instead of adding the class to the body, navbar height is dynamically calculated and proper styles are added .
  */
 function handleFixedNavbarPosition() {
-    const navbar = document.querySelector('.navbar');
-    const shouldPageHaveNavbarPadding = !document.body.classList.contains('no-navbar-padding');
-    if (navbar && shouldPageHaveNavbarPadding) {
-        const navbarHeight = navbar.getBoundingClientRect().height;
-        if (navbar.classList.contains('is-fixed-top')) {
-            document.body.style.paddingTop = `${navbarHeight}px`;
-        } else if (navbar.classList.contains('is-fixed-bottom')) {
-            document.body.style.paddingBottom = `${navbarHeight}px`;
-        }
+  const navbar = document.querySelector('.navbar');
+  const shouldPageHaveNavbarPadding =
+    !document.body.classList.contains('no-navbar-padding');
+  if (navbar && shouldPageHaveNavbarPadding) {
+    const navbarHeight = navbar.getBoundingClientRect().height;
+    if (navbar.classList.contains('is-fixed-top')) {
+      document.body.style.paddingTop = `${navbarHeight}px`;
+    } else if (navbar.classList.contains('is-fixed-bottom')) {
+      document.body.style.paddingBottom = `${navbarHeight}px`;
     }
+  }
+}
+
+/**
+ * Megamenu stays open when mouse is on navbar-menu element.
+ * It prevents from closing early eg. when moving mouse to distant link inside mega dropdown
+ */
+function handleMegamenuHover(isDesktop) {
+  const menu = document.querySelector('.navbar-menu');
+  const allNavbarItems = Array.from(
+    document.querySelectorAll('.navbar-end > .navbar-item')
+  ); // only first level
+  const itemsWithMegamenuDropdown = allNavbarItems.filter((item) =>
+    item
+      .querySelector('.navbar-dropdown')
+      ?.classList.contains('navbar-mega-dropdown')
+  );
+
+  const hasMegaDropdown = (item: HTMLElement) =>
+    item
+      .querySelector('.navbar-dropdown')
+      ?.classList.contains('navbar-mega-dropdown');
+
+  const closeMegamenu = () => {
+    itemsWithMegamenuDropdown.forEach((item) =>
+      item.classList.remove('is-hovered')
+    );
+  };
+
+  const openMegamenu = (item: HTMLElement) => {
+    item.classList.add('is-hovered');
+  };
+
+  const handleHover = (event) => {
+    closeMegamenu();
+    openMegamenu(event.target);
+  };
+
+  if (isDesktop) {
+    allNavbarItems.forEach((item: HTMLElement) => {
+      if (hasMegaDropdown(item)) {
+        item.addEventListener('mouseenter', handleHover);
+      } else {
+        item.addEventListener('mouseenter', closeMegamenu);
+      }
+    });
+
+    menu.addEventListener('mouseleave', closeMegamenu);
+  } else {
+    allNavbarItems.forEach((item: HTMLElement) => {
+      if (hasMegaDropdown(item)) {
+        item.removeEventListener('mouseenter', handleHover);
+      } else {
+        item.removeEventListener('mouseenter', closeMegamenu);
+      }
+    });
+    menu.removeEventListener('mouseleave', closeMegamenu);
+  }
 }
