5e04c13828854af9cc6aff65abea2931af50d2a0
KorzunYuri <korzun.yuri@gmail.com>
2024-04-26 10:36:58 +0200

    KYAN-183 update ExperienceFragment to autofill resource path

--- applications/fragments/backend/src/main/java/pl/ds/kyanite/fragments/components/models/ExperienceFragment.java
+++ applications/fragments/backend/src/main/java/pl/ds/kyanite/fragments/components/models/ExperienceFragment.java
@@ -16,15 +16,27 @@
 
 package pl.ds.kyanite.fragments.components.models;
 
+import java.util.ArrayList;
+import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
+import javax.jcr.Node;
+import javax.jcr.NodeIterator;
+import javax.jcr.Session;
+import javax.jcr.query.Query;
+import javax.jcr.query.QueryManager;
+import javax.jcr.query.QueryResult;
 import lombok.Getter;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.sling.api.resource.ModifiableValueMap;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.api.resource.ValueMap;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.Self;
 import org.apache.sling.models.annotations.injectorspecific.SlingObject;
+import pl.ds.kyanite.fragments.components.utils.PagesSpaceUtil;
 
 @Model(adaptables = Resource.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
 public class ExperienceFragment {
@@ -33,17 +45,27 @@ public class ExperienceFragment {
   @Getter
   private String resource;
 
+  @Inject
+  private String referenceResourceType;
+
+  @Inject
+  String referenceResourceName;
+
   @Getter
   private Boolean validPage = false;
 
   @Getter
   private String pagePath;
 
+  @Self
+  private Resource resourceObject;
+
   @SlingObject
   private ResourceResolver resourceResolver;
 
   @PostConstruct
   private void init() {
+    resolveReferenceResource();
     validPage = checkIfValidPage();
     pagePath = preparePagePath();
   }
@@ -72,4 +94,48 @@ public class ExperienceFragment {
     return path + ".html";
   }
 
+  private void resolveReferenceResource() {
+    if (StringUtils.isBlank(resource)) {
+      List<String> clauses = new ArrayList<>();
+      if (StringUtils.isNotBlank(referenceResourceType)) {
+        clauses.add(String.format(" AND c.[sling:resourceType] = '%s'", referenceResourceType));
+      }
+      if (StringUtils.isNotBlank(referenceResourceName)) {
+        clauses.add(String.format(" AND NAME(page) = '%s'", referenceResourceName));
+        if (StringUtils.isBlank(referenceResourceType)) {
+          clauses.add(
+              " AND (     c.[sling:resourceType]      = 'kyanite/fragments/components/page'"
+                  + " OR  c.[sling.resourceSuperType] = 'kyanite/fragments/components/page'"
+                  + ")");
+        }
+      }
+      if (!clauses.isEmpty()) {
+        try {
+          Session session = resourceResolver.adaptTo(Session.class);
+          QueryManager queryManager = session.getWorkspace().getQueryManager();
+          String queryString = String.format(
+              " SELECT page.* "
+                  + " FROM [ws:Page] AS page"
+                  + " INNER JOIN [ws:PageContent] AS c "
+                  + "     ON ISCHILDNODE(c, page) "
+                  + " WHERE ISDESCENDANTNODE(page, '/content/%s') "
+                  + " %s",
+              PagesSpaceUtil.getWsPagesSpaceName(resourceObject),
+              String.join(" ", clauses));
+          Query query = queryManager.createQuery(queryString, Query.JCR_SQL2);
+          QueryResult result = query.execute();
+          NodeIterator nodeIterator = result.getNodes();
+          if (nodeIterator.hasNext()) {
+            Node node = nodeIterator.nextNode();
+            resource = node.getPath();
+            ModifiableValueMap vm = resourceObject.adaptTo(ModifiableValueMap.class);
+            vm.put("resource", resource);
+            session.save();
+          }
+        } catch (Exception ignored) {
+          // ignore the error, leave the field to manual authoring
+        }
+      }
+    }
+  }
 }
---
+++ applications/fragments/backend/src/main/java/pl/ds/kyanite/fragments/components/utils/PagesSpaceUtil.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (C) 2024 Dynamic Solutions
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package pl.ds.kyanite.fragments.components.utils;
+
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
+
+public class PagesSpaceUtil {
+
+  private static Resource getSpace(String resourcePath, ResourceResolver resourceResolver) {
+    String spacePath = getCurrentSpace(resourcePath);
+    if (spacePath == null) {
+      return null;
+    }
+
+    return resourceResolver.getResource(spacePath);
+  }
+
+  public static String getWsPagesSpaceName(Resource resource) {
+    Resource pagesSpace = getSpace(resource.getPath(), resource.getResourceResolver());
+    if (pagesSpace == null) {
+      return StringUtils.EMPTY;
+    }
+
+    return pagesSpace.getParent().getName();
+  }
+
+  private static String getCurrentSpace(String resourcePath) {
+    String regex = "^((/content|/published)/[^/]*/pages/).*";
+    if (StringUtils.isNotBlank(resourcePath)) {
+      final Pattern pattern = Pattern.compile(regex);
+      final Matcher matcher = pattern.matcher(resourcePath);
+
+      if (matcher.find()) {
+        return matcher.group(1);
+      }
+    }
+    return null;
+  }
+
+  private PagesSpaceUtil() {
+    // no instance
+  }
+
+}
