0bdda6d892bea1a475244c085fdebb1554dfcc28
martyna-ds <120561838+martyna-ds@users.noreply.github.com>
2024-06-13 15:21:55 +0200

    Kyan 245 hero dynamic title text wrap improvements needed (#472)
    * KYAN-245 add option to dialog with no wrap - instead text shrinking by testing it's height
    
    * KYAN-245 add option to dialog with no wrap - parse boolean value

--- applications/common/frontend/src/components/Title/animatedEndings.class.ts
+++ applications/common/frontend/src/components/Title/animatedEndings.class.ts
@@ -14,13 +14,43 @@
  * limitations under the License.
  */
 
+import { breakpoints } from 'src/helpers/breakpoints';
 import Typed from 'typed.js';
+
+/**
+ * Helper function which takes the longest from all endings and check in each iteration if it fits in one line.
+ * It uses already rendered endings (in html for SEO purposes) to take the longest element and test height.
+ *
+ * @param titleEl Title html element to which font-size is applied
+ * @param currentSize current size of the title element, desreasing in each iteration by one pixel
+ * @param lineHeight Desired height of the animated ending (max height)
+ *
+ */
+const adjustTitleFontSize = (
+  titleEl: HTMLElement,
+  currentSize: number,
+  lineHeight: number
+) => {
+  titleEl.style.fontSize = currentSize + 'px';
+  const heights = Array.from(
+    titleEl.querySelectorAll('.container-with-endings > span')
+  ).map((ending) => {
+    return ending.clientHeight;
+  });
+  const longestEnding = Math.max(...heights);
+  if (longestEnding > lineHeight) {
+    adjustTitleFontSize(titleEl, currentSize - 1, lineHeight);
+  } else {
+    return;
+  }
+};
 
 export class AnimatedEndings {
   static readonly SELECTOR = '.typed-animation';
   public readonly element: HTMLElement;
   public readonly titleElement: HTMLElement;
   public readonly showCursor: boolean;
+  public readonly noTitleWrapping: boolean;
   public readonly speed: number;
   public readonly delay: number;
   public readonly strings: string[];
@@ -29,6 +59,7 @@ export class AnimatedEndings {
     this.element = element;
     this.titleElement = title;
     this.showCursor = JSON.parse(element.dataset.showCursor);
+    this.noTitleWrapping = JSON.parse(element.dataset.noTitleWrapping);
     this.speed = Number(element.dataset.speed);
     this.delay = Number(element.dataset.delay);
     this.strings = JSON.parse(element.dataset.endings);
@@ -50,10 +81,16 @@ export class AnimatedEndings {
       },
     });
 
-    AnimatedEndings.setHeightOfTitle(this.titleElement); // will be overwritten in onload event (to avoid jumping, onload needed for Safari bug)
-    window.onresize = () => {
-      AnimatedEndings.setHeightOfTitle(this.titleElement);
-    };
+    if (this.noTitleWrapping) {
+      window.onresize = () => {
+        AnimatedEndings.setFontSize(this.titleElement);
+      };
+    } else {
+      AnimatedEndings.setHeightOfTitle(this.titleElement); // will be overwritten in onload event (to avoid jumping, onload needed for Safari bug)
+      window.onresize = () => {
+        AnimatedEndings.setHeightOfTitle(this.titleElement);
+      };
+    }
   }
 
   static setHeightOfTitle(titleEl: HTMLElement) {
@@ -65,5 +102,20 @@ export class AnimatedEndings {
     const titleContainer: HTMLElement =
       titleEl.querySelector('.title-container');
     titleContainer.style.height = Math.max(...heights) + 'px';
+  }
+
+  static setFontSize(titleEl: HTMLElement) {
+    if (window.matchMedia(`(max-width: ${breakpoints.md}px)`).matches) {
+      const initialSize = Number(
+        window.getComputedStyle(titleEl).fontSize.split('px')[0]
+      );
+      const lineHeight = Number(
+        window.getComputedStyle(titleEl).lineHeight.split('px')[0]
+      );
+
+      if (!isNaN(initialSize) && !isNaN(lineHeight)) {
+        adjustTitleFontSize(titleEl, initialSize, lineHeight);
+      }
+    }
   }
 }
