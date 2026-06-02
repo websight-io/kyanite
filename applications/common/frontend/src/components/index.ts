a77d1b881ba39484172da6b466d169d240d5e717
Tomasz Dziezyk <tomasz.dziezyk@ds.pl>
2026-06-02 14:34:15 +0200

    [KYAN-340] Formatting

--- applications/common/frontend/src/components/index.ts
+++ applications/common/frontend/src/components/index.ts
@@ -13,7 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 import "@algolia/autocomplete-theme-classic";
+
 import './CodeSnippet/codesnippet';
 import './ContactForm/contactForm';
 import './Dropdown/dropdown';
